4060f3108b6823356e30f515ba80a09b9aebe859
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-09-05 16:36:03 +0200

    wait i was supposed to do this?

--- api/hook.js
+++ api/hook.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { payload } = req.body;
 
-  console.log(payload); // what the fuck are you doing handler
+  console.log(JSON.stringify(payload, null, 2));  // what the fuck are you doing handler
 
   const discordRes = await fetch(process.env.DISCORD_WEBHOOK, {
     method: "POST",
