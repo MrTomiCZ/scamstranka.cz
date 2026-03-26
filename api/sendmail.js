660b689710ab4616f9c03759db2311e356dae96d
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-03-26 15:37:06 +0100

    Update sendmail.js

--- api/sendmail.js
+++ api/sendmail.js
@@ -1,19 +1,22 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
+    //get delete patch atd
     if (req.method !== 'POST') {
         res.status(200).json({ error: '405 Nepovolena metoda' });
         return;
     }
 
+    //si ziskame body
     const { name, email, message } = req.body;
 
+    //nejsou body veci
     if (!name || !email || !message) {
         res.status(200).json({ error: '400 Chybi potrebne moznosti' });
         return;
     }
 
-    // ja nevim ale tadytim se posila mejl
+    // credentials
     const transporter = nodemailer.createTransport({
         host: 'smtp.seznam.cz',
         port: 465,
@@ -25,14 +28,17 @@ export default async function handler(req, res) {
     });
 
     try {
+        //vodeslat
         await transporter.sendMail({
             from: "SCAMSTRANKA MAILER <"+process.env.SEZNAM_USER+">",
-            to: 'mrtomicz@frdomains.eu; kontakt@scamstranka.cz',
+            to: 'scamstranka@mrtomicz.eu kontakt@scamstranka.cz',
             subject: "[KONTAKT FORM] Nová zpráva > " + name,
             html: "Jméno: <strong>" + name + "</strong><br>Email: <strong><a href="+`"${email}">` + email + "</a></strong><br>Zpráva: <strong>" + message + "</strong><br>IP Adresa: <strong>" + (req.body.ip || 'N/A') + "</strong>",
         });
+        //potvrdime uzivateli
         res.status(200).json({ message: '200 Odeslano v poradku' });
     } catch (error) {
+        //neco pokazilo sa
         res.status(200).json({ error: '500 Nelze odeslat email', details: error.message });
     }
 }
